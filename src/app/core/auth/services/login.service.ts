ba3ea74657558c18937c0bc7c7ac4b02d8c59f4d
Kerolos2000 <kero.magdy2000@gmail.com>
2026-03-15 23:06:52 +0200

    feat: 🎸 Implement user logout service and a dropdown navbar component for user actions

--- src/app/core/auth/services/login.service.ts
+++ src/app/core/auth/services/login.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ROUTES } from '../../constants/routes';
 import { environment } from '../../environments/environment';
 import { AuthSuccessResponse } from '../models/user.interface';
 
@@ -8,11 +10,18 @@ import { AuthSuccessResponse } from '../models/user.interface';
 })
 export class LoginService {
   private readonly http = inject(HttpClient);
+  private readonly router = inject(Router);
 
   login(data: object) {
     return this.http.post<AuthSuccessResponse>(
       `${environment.BASE_URL}/users/signin`,
       data,
     );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.router.navigateByUrl(ROUTES.LOGIN);
   }
 }
--- src/app/layouts/main-layout/components/dropdown-navbar/dropdown-navbar.component.ts
+++ src/app/layouts/main-layout/components/dropdown-navbar/dropdown-navbar.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LoginService } from '../../../../core/auth/services/login.service';
 
 @Component({
   selector: 'app-dropdown-navbar',
   imports: [RouterLink],
   templateUrl: './dropdown-navbar.component.html',
 })
-export class DropdownNavbarComponent {}
+export class DropdownNavbarComponent {
+  private readonly loginService = inject(LoginService);
+
+  logout() {
+    this.loginService.logout();
+  }
+}
